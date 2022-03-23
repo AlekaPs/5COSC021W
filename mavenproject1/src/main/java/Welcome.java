b4eaece476cb67e3acec0f9e742b350096274338
aleka <psarroa@westminster.ac.uk>
2022-03-23 22:54:30 +0000

    First file in Java

---
+++ mavenproject1/src/main/java/Welcome.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aleka
+ */
+public class Welcome {
+    public static void main(String[] args)
+    {
+        String greetings = "Welcome";
+        System.out.println(greetings);
+    
+    
+    }
+}
